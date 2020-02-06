617c5c5effb45e7ed18416ff9cba5834af53da1f
denis <denis.grin1618@gmail.com>
2020-02-06 17:29:31 +0200

    add users.php

---
+++ users.php
@@ -0,0 +1,10 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "");
+
+if ($link == false){
+    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+}
+else {
+    print("Соединение установлено успешно");
+}
