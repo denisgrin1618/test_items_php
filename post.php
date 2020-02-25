8fe8ffa2853e7bcf61a63b3081ae216eca6a6697
denis <denis.grin1618@gmail.com>
2020-02-25 19:46:54 +0200

    merge branch_2

--- post.php
+++ post.php
@@ -3,11 +3,4 @@
 $post_data = file_get_contents('php://input');
 echo $post_data;
 echo htmlspecialchars($_POST['username']);
-<<<<<<< HEAD
-echo "branch_2 2222222222222222222222222222";
-echo "branch_2 444466";
-=======
-echo "branch_2";
-echo "master";
 
->>>>>>> new masege
