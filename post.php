8683b3fb4f289ef1c031f5c8702cad39335ad1cd
denis <denis.grin1618@gmail.com>
2020-02-25 19:21:59 +0200

    new masege

--- post.php
+++ post.php
@@ -4,3 +4,11 @@ $post_data = file_get_contents('php://input');
 echo $post_data;
 echo htmlspecialchars($_POST['username']);
 
+
+echo "branch_2 2222222222222222222222222222";
+echo "branch_2 444466";
+
+echo "branch_2";
+echo "master";
+
+
